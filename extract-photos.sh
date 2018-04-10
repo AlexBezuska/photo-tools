b034550dfa9080b29432566ffdc9903deaafb31e
Alex Bezuska <abezuska@gmail.com>
2018-04-09 21:26:49 -0400

    Fix bug in extract-photos

--- extract-photos.sh
+++ extract-photos.sh
@@ -10,16 +10,16 @@ if [ -z "$1" ]; then
 else
 
 # This could be done much better 
-find "${1}" -name '*.jpg' -exec gmv -v {} photoDestination \;
-find "${1}" -name '*.JPG' -exec gmv -v {} photoDestination \;
-find "${1}" -name '*.jpeg' -exec gmv -v {} photoDestination \;
-find "${1}" -name '*.png' -exec gmv -v {} photoDestination \;
-find "${1}" -name '*.PNG' -exec gmv -v {} photoDestination \;
+find "${1}" -name '*.jpg' -exec gmv -v {} $photoDestination \;
+find "${1}" -name '*.JPG' -exec gmv -v {} $photoDestination \;
+find "${1}" -name '*.jpeg' -exec gmv -v {} $photoDestination \;
+find "${1}" -name '*.png' -exec gmv -v {} $photoDestination \;
+find "${1}" -name '*.PNG' -exec gmv -v {} $photoDestination \;
 
 
-find "${1}" -name '*.mov' -exec gmv -v {} videoDestination \;
-find "${1}" -name '*.MOV' -exec gmv -v {} videoDestination \;
-find "${1}" -name '*.mp4' -exec gmv -v {} videoDestination \;
-find "${1}" -name '*.m4v' -exec gmv -v {} videoDestination \;
-find "${1}" -name '*.avi' -exec gmv -v {} videoDestination \;
-find "${1}" -name '*.AVI' -exec gmv -v {} videoDestination \;
+find "${1}" -name '*.mov' -exec gmv -v {} $videoDestination \;
+find "${1}" -name '*.MOV' -exec gmv -v {} $videoDestination \;
+find "${1}" -name '*.mp4' -exec gmv -v {} $videoDestination \;
+find "${1}" -name '*.m4v' -exec gmv -v {} $videoDestination \;
+find "${1}" -name '*.avi' -exec gmv -v {} $videoDestination \;
+find "${1}" -name '*.AVI' -exec gmv -v {} $videoDestination \;
