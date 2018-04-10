e9a557ff767a5e52618fe35fe66f8a5ef84e0328
Alex Bezuska <abezuska@gmail.com>
2018-04-10 11:31:34 -0400

    fix error on ename-photos

--- rename-photos.sh
+++ rename-photos.sh
@@ -6,6 +6,7 @@ if [ -z "$1" ]; then
   echo "Please add a path to a folder containing images\n Example: \n sh rename-photos /my/folder/ \n"
 else
 
+  ## rename photos to include cameraname and date photo was taken
+  exiftool '-filename<${datetimeoriginal} ${model;}.%e' -d "%Y-%m-%d %H.%M.%S%%-c" "${1}"
 
-## rename photos to include cameraname and date photo was taken
-exiftool '-filename<${datetimeoriginal} ${model;}.%e' -d "%Y-%m-%d %H.%M.%S%%-c" "${1}"
+fi
